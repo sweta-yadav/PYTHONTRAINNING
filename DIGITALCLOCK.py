34d2a7d2436d9213ddef5ef7d21ad306cdb2c932
sweta <swetayadav2060@gmail.com>
2025-08-25 18:49:17 +0530

    added

--- DIGITALCLOCK.py
+++ DIGITALCLOCK.py
@@ -1,28 +1,59 @@
-import time
-import os
+# import time
+# import os
 
-def digital_clock():
-    h = 3
-    m = 28
-    s = 0
+# def digital_clock():
+#     h = 3
+#     m = 28
+#     s = 0
     
-    while True:
-        # Print time in HH:MM:SS format
-        print(f"\n\n\t\t {h:02d}:{m:02d}:{s:02d}")
+#     while True:
         
-        time.sleep(1)   # wait for 1 second
-        os.system("cls")  # clear screen (Windows). Use "clear" on Linux/Mac.
+#         print(f"\n\n\t\t {h:02d}:{m:02d}:{s:02d}")
         
-        # Update seconds, minutes, hours
-        s += 1
-        if s == 60:
-            m += 1
-            s = 0
-        if m == 60:
-            h += 1
-            m = 0
-        if h == 12:
-            h = 0
+#         time.sleep(1)   
+#         os.system("cls")  
+        
+        
+#         s += 1
+#         if s == 60:
+#             m += 1
+#             s = 0
+#         if m == 60:
+#             h += 1
+#             m = 0
+#         if h == 12:
+#             h = 0
 
-# Run clock
-digital_clock()
+
+# digital_clock()
+
+
+
+
+#METHOD 2
+
+import time
+import sys
+
+h = 12   
+m = 20   
+s = 0    
+
+while True:
+    
+    sys.stdout.write(f"\r{h:02d}:{m:02d}:{s:02d}")
+    sys.stdout.flush()
+
+    time.sleep(1)  
+    s += 1
+
+    if s == 60:   
+        s = 0
+        m += 1
+
+    if m == 60:   
+        m = 0
+        h += 1
+
+    if h == 24:   
+        h = 0
